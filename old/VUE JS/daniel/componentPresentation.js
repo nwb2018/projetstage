316fa982d0293b28463ab3a0221de3c0a5d69973
nwb237 <nguedjiobilly@gmail.com>
2018-08-20 15:29:02 +0200

    onglet présentation

--- old/VUE JS/daniel/componentPresentation.js
+++ old/VUE JS/daniel/componentPresentation.js
@@ -1,11 +1,10 @@
 const PresentationComponent = {
     template: `
-    <div class="all-border-black column" style="background: url(image/zz.jpg)">
+    <div class="rr">
 PRESENTATION
-<div class="row all-border-black" style="height: 200px; background-color: white; "> HISTORIQUE</div>
-<div class="row all-border-black" style="height: 200px; background-color: white;"> NOS FILIERES</div>
-<div class="row all-border-black" style="height: 200px; background-color: white;">
-    <div class="all-border-black" >Poser Question </div>
-    <div class="all-border-black flex-1-1">INFOS </div></div>
-</div>`
+<section class="first"> 
+<div class="new"></div>
+</section>
+<section class="second"></section>
+<section class="last"><div class="new2"></div></section>`
 };
